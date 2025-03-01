e4bfdf1c8ffded9bfd36561c5fe1a41897c60f00
Mth_Darlyson <matheussdarlyson@gmail.com>
2025-02-28 21:58:46 -0300

    first

---
+++ src/main/java/dio/aula_spring_data_jpa/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package dio.aula_spring_data_jpa.repository;
+
+import dio.aula_spring_data_jpa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  UserRepository extends JpaRepository<User, Integer> {
+
+}
